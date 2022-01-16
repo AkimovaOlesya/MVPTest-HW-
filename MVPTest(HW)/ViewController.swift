daf2b8ecc5180a8f7e235fc542e21670073393db
AkimovaOlesya <lesya.akimova93@gmail.com>
2022-01-16 20:33:57 +0700

    Initial Commit

---
+++ MVPTest(HW)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPTest(HW)
+//
+//  Created by Леся Акимова on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
